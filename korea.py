aa42e9cc34dd6e8ef147b15f0f922bc81f0866ec
ses1430 <ses1430@gmail.com>
2022-12-09 21:33:39 +0900

    .

--- korea.py
+++ korea.py
@@ -3,40 +3,30 @@ import numpy as np
 import FinanceDataReader as fdr
 import datetime as dt
 import subprocess
+import ta
 
-kdrx = {
-     '003410':'쌍용C&E'
-    ,'305720':'K.2차전지'
-    ,'305540':'T.2차전지'    
-    ,'379800':'S&P500'
-    ,'379810':'나스닥'
-    ,'381170':'테크Top10'    
-    ,'394670':'글로벌리튬'
-    ,'381180':'미국필라반'
-    ,'390390':'미국반도체'
-    ,'371460':'차이나전기차'
-    ,'396510':'차이나클린'
-    ,'396520':'차이나반도체'    
-    ,'400570':'유럽탄소'
-    ,'228810':'미디어컨텐츠'
-    ,'263750':'펄어비스'
-    ,'259960':'크래프톤'
-    ,'194480':'데브시스터즈'
-}
+stocks = {item.split('\t')[0]:item.split('\t')[1] for item in open('kor_ticker.dat', 'r', encoding='utf-8').readlines()}
 
-start_dt = (dt.datetime.now() - dt.timedelta(days=5)).strftime('%Y-%m-%d')
+start_dt = (dt.datetime.now() - dt.timedelta(days=1850)).strftime('%Y-%m-%d')
 end_dt = dt.datetime.now().strftime('%Y-%m-%d')
 
-items = list(kdrx.keys())
-
-df_list = [fdr.DataReader(t, start_dt, end_dt)['Close'] for t in items]
+df_list = [fdr.DataReader(t, start_dt, end_dt)['Close'] for t in stocks]
 df = pd.concat(df_list, axis=1)
-df.columns = list(kdrx.values())
-df = df.replace({np.nan: None})
+df.columns = list(stocks.values())
+#df = data.replace({np.nan: None})
+
+rsi = {}
+for column in df.columns:
+    rsi[column] = ta.momentum.rsi(df[column])[-1]
+
+df = pd.concat([df, pd.DataFrame(data=[rsi])]).iloc[::-1].T
+df.rename(columns={df.columns[0]:'RSI'}, inplace=True)
 
 writer = pd.ExcelWriter('kdrx.xlsx', engine='xlsxwriter')
 
 #최근 120일치만 가져오기
-df.iloc[::-1].to_excel(writer, sheet_name='Sheet1')
+df.to_excel(writer, sheet_name='Sheet1')
 writer.close()
-subprocess.call("C:\Program Files (x86)\\Microsoft Office\\root\\Office16\\EXCEL.EXE kdrx.xlsx")
+
+# subprocess.call("C:\\Program Files\\Microsoft Office 15\\root\\office15\\EXCEL.EXE kdrx.xlsx")
+subprocess.call("C:\\Program Files\\Microsoft Office 15\\root\\office15\\EXCEL.EXE kdrx.xlsx")
--- price.py
+++ price.py
@@ -26,4 +26,6 @@ df.rename(columns={df.columns[0]:'RSI'}, inplace=True)
 writer = pd.ExcelWriter('price.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.close()
-subprocess.call("C:\Program Files (x86)\\Microsoft Office\\root\\Office16\\EXCEL.EXE price.xlsx")
+
+# subprocess.call("C:\Program Files (x86)\\Microsoft Office\\root\\Office16\\EXCEL.EXE price.xlsx")
+subprocess.call("C:\\Program Files\\Microsoft Office 15\\root\\office15\\EXCEL.EXE price.xlsx")
