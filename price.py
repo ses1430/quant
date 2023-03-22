5c22c88fbd0a39f286956ceb5587afdb3942af67
Im Hyeong-seob <ses1430@gmail.com>
2023-03-22 17:12:11 +0900

    eth, sma 추가

--- price.py
+++ price.py
@@ -28,9 +28,6 @@ for ticker in stocks:
     stat[ticker] = {}
     t = data['Close'][ticker]
 
-    stat[ticker]['RSI'] = ta.momentum.rsi(t)[-1]
-    stat[ticker]['BB.P'] = ta.volatility.bollinger_pband(t, window, window_dev, True)[-1] * 100
-
     sma120 = ta.trend.sma_indicator(t, window=120, fillna=True)[-1]
     sma200 = ta.trend.sma_indicator(t, window=200, fillna=True)[-1]
 
@@ -38,6 +35,9 @@ for ticker in stocks:
     # stat[ticker]['SMA200'] = sma200
     stat[ticker]['SMA120'] = t[-1] / sma120
     stat[ticker]['SMA200'] = t[-1] / sma200
+
+    stat[ticker]['RSI'] = ta.momentum.rsi(t)[-1]
+    stat[ticker]['BB.P'] = ta.volatility.bollinger_pband(t, window, window_dev, True)[-1] * 100    
 
 df_stat = pd.DataFrame(data=stat)[::-1]
 df = pd.concat([df, df_stat]).iloc[::-1].T
