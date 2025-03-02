b9dbaad4d67375c0fd267e7aba4d7a7362f1a8fd
Im Hyeong-seob <ses1430@gmail.com>
2025-03-03 00:10:22 +0900

    시총 USD환산

--- upbit.py
+++ upbit.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 hist_top_btc = 152558528
-btc_amt = 0.40696224
+btc_amt = 0.41900825
 
 def get_ticker_price(ticker):
     url = "https://api.upbit.com/v1/ticker?markets=" + ticker
